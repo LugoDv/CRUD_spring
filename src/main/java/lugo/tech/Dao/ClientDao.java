7c099eb31fc2d6a7765c9e1b0fae6bcbbf583c36
LugoDv <lugopersonal@gmail.com>
2025-06-22 00:32:07 +0200

    config: css style from spring

---
+++ src/main/java/lugo/tech/Dao/ClientDao.java
@@ -0,0 +1,11 @@
+package lugo.tech.Dao;
+
+import java.util.List;
+
+import lugo.tech.controlador.entity.Cliente;
+
+public interface ClientDao {
+
+	public List<Cliente> getAllClients();
+	
+}
--- src/main/java/lugo/tech/controller/ClientController.java
+++ src/main/java/lugo/tech/controller/ClientController.java
@@ -1,15 +1,28 @@
 package lugo.tech.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lugo.tech.Dao.ClientDao;
+import lugo.tech.controlador.entity.Cliente;
 
 @Controller
 @RequestMapping("/cliente")
 public class ClientController {
 
+	@Autowired
+	private ClientDao clientDao;
+	
 	@RequestMapping("/lista")
 	public String clientList (Model model) {
+		
+		List<Cliente> listClients = clientDao.getAllClients();
+		
+		model.addAttribute("clients",listClients);
 		
 		return "clientList"; // Retorna el nombre de la vista que se va a renderizar
 	}
